b2880fd2b6f540f4b6b0bae112561f30930e8428
michvier <github@vierhauser.net>
2019-01-20 16:14:56 -0500

    Create run_dronology.sh

---
+++ scripts/run_dronology.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+#cd "$(dirname "$(realpath "$0")")"
+################################ MODIFY DRONOLOGY REPO PARAMETERS HERE ##############################
+REPO=/home/uav/git/Dronology/
+server=edu.nd.dronology.services.launch
+vaadin=edu.nd.dronology.ui.vaadin
+################################
+
+pkill -9 java
+#$cd $RPEO; exec mvn install"
+
+gnome-terminal --working-directory=$REPO/$server -x bash -c "mvn exec:java"
+gnome-terminal --working-directory=$REPO/$vaadin -x bash -c "mvn jetty:run"
+sleep 15
+xdg-open 'http://localhost:8080/vaadinui'
