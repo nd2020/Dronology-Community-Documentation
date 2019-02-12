69396a7f7efed9c4411cce81c94b7eb24338ca5c
michvier <github@vierhauser.net>
2019-02-12 14:03:54 -0500

    Update setup_dronology-GCS.sh

--- scripts/setup_dronology-GCS.sh
+++ scripts/setup_dronology-GCS.sh
@@ -31,7 +31,7 @@ sudo apt-get install --yes python-qt4
 # makes the directory if it doesn't already exist
 mkdir -p "$REPOS_PARENT_DIR/$REPOS_DIR"
 cd "$REPOS_PARENT_DIR/$REPOS_DIR"
-git clone $REPO_URL $REPO_NAME
+git clone -b $BRANCH --single-branch  $REPO_URL $REPO_NAME
 cd $REPO_NAME
 git checkout $BRANCH
 cd ..
